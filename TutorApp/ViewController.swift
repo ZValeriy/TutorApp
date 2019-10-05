13ecee93e5ff5ba65957240ecc57d9918350f3cb
Valeriy <valeriyzubairov@gmail.com>
2019-10-05 21:40:53 +0300

    First Commit

---
+++ TutorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TutorApp
+//
+//  Created by Valeriy on 05.10.2019.
+//  Copyright © 2019 Валерий Зубаиров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
