bb34a29d28aad317aa60720b77ff72a92530b6c1
Valeriy <valeriyzubairov@gmail.com>
2019-11-25 01:11:11 +0300

    dismissKeyboard

--- TutorApp/ViewControllers/SignIn/SignInViewController.swift
+++ TutorApp/ViewControllers/SignIn/SignInViewController.swift
@@ -11,16 +11,44 @@ import Foundation
 import Firebase
 
 class SignInViewController: UIViewController {
+    
 
     @IBOutlet weak var loginField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var constraintContentHeight: NSLayoutConstraint!
+    
+    var activeField: UITextField?
+    var lastOffset: CGPoint!
+    var keyboardHeight: CGFloat!
     var ref: DatabaseReference!
     
+    @IBAction func dismissKeyboardOnLogin(_ sender: Any) {
+        loginField.resignFirstResponder()
+    }
+    
+    @IBAction func dismissKeyboardOnPassword(_ sender: Any) {
+        passwordField.resignFirstResponder()
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: Any) {
+        loginField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+    }
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        
+        loginField.delegate = self
+        passwordField.delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(SignInViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(SignInViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        
+        
         addBorder(to: loginField)
         addBorder(to: passwordField)
         activityIndicator.stopAnimating()
@@ -72,14 +100,71 @@ class SignInViewController: UIViewController {
     @IBAction func passwordChanging(_ sender: Any) {
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func returnTextView(gesture: UIGestureRecognizer) {
+        guard activeField != nil else {
+            return
+        }
+        
+        activeField?.resignFirstResponder()
+        activeField = nil
     }
-    */
 
+
+}
+
+extension SignInViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        activeField = textField
+        lastOffset = self.scrollView.contentOffset
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        activeField?.resignFirstResponder()
+        activeField = nil
+        return true
+    }
+}
+
+// MARK: Keyboard Handling
+extension SignInViewController {
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if keyboardHeight != nil {
+            return
+        }
+        
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            keyboardHeight = keyboardSize.height
+            
+            // so increase contentView's height by keyboard height
+            UIView.animate(withDuration: 0.1, animations: {
+                self.constraintContentHeight.constant += self.keyboardHeight
+            })
+            
+            // move if keyboard hide input field
+            let distanceToBottom = self.scrollView.frame.size.height - (activeField?.frame.origin.y)! - (activeField?.frame.size.height)!
+            let collapseSpace = keyboardHeight - distanceToBottom
+            
+            if collapseSpace < 0 {
+                // no collapse
+                return
+            }
+            
+            // set new offset for scroll view
+            UIView.animate(withDuration: 0.3, animations: {
+                // scroll to the position above keyboard 10 points
+                self.scrollView.contentOffset = CGPoint(x: self.lastOffset.x, y: collapseSpace + 10)
+            })
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        UIView.animate(withDuration: 0.3) {
+            self.constraintContentHeight.constant -= self.keyboardHeight
+            
+            self.scrollView.contentOffset = self.lastOffset
+        }
+        
+        keyboardHeight = nil
+    }
 }
